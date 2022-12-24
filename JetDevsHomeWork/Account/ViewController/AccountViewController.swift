276ebe549baed5087e169e5c56977c384f1501a2
Developer <viralbhalala@gmail.com>
2022-12-24 09:40:34 +0530

    Initial Code commit

---
+++ JetDevsHomeWork/Account/ViewController/AccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountViewController.swift
+//  JetDevsHomeWork
+//
+//  Created by Gary.yao on 2021/10/29.
+//
+
+import UIKit
+import Kingfisher
+
+class AccountViewController: UIViewController {
+
+	@IBOutlet weak var nonLoginView: UIView!
+	@IBOutlet weak var loginView: UIView!
+	@IBOutlet weak var daysLabel: UILabel!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var headImageView: UIImageView!
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+		self.navigationController?.navigationBar.isHidden = true
+        // Do any additional setup after loading the view.
+		nonLoginView.isHidden = false
+		loginView.isHidden = true
+    }
+	
+	@IBAction func loginButtonTap(_ sender: UIButton) {
+	}
+	
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+platform :ios, '12.0'
+use_frameworks!
+
+inhibit_all_warnings!
+
+target 'JetDevsHomeWork' do
+  pod 'RxSwift', '~> 5.1.0'
+  pod 'SwiftLint'
+  pod 'Kingfisher'
+  pod 'SnapKit'
+
+  target 'JetDevsHomeWorkTests' do
+    inherit! :search_paths
+  end
+
+  target 'JetDevsHomeWorkUITests' do
+    inherit! :complete
+  end  
+
+end
+
